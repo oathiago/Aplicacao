5b59e669c6ea93ddda5edf9a74cf20549e8e78f5
oathiago <oa.thiago@gmail.com>
2019-11-02 13:57:08 -0300

    Retirada de metodos não utilizaveis

--- src/main/java/com/br/codenation/application/domain/entity/Company.java
+++ src/main/java/com/br/codenation/application/domain/entity/Company.java
@@ -1,11 +1,12 @@
 package com.br.codenation.application.domain.entity;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
-import java.util.ArrayList;
 import java.util.List;
 
 @Data
