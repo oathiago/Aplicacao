72062ed7f1b2a8305cc42ccaf9904d2ab7b7f3cc
oathiago <TAO*25#12@tao>
2019-11-16 23:05:15 -0300

    remove configuration

--- src/main/java/com/br/codenation/application/Application.java
+++ src/main/java/com/br/codenation/application/Application.java
@@ -6,13 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableAutoConfiguration
 @EnableScheduling
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-
-
 }
